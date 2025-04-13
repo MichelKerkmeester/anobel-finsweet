56965eecf438920c045a44ca352a40ddc3254cb2
Michel Kerkmeester <82775228+MichelKerkmeester@users.noreply.github.com>
2025-04-13 17:51:55 +0200

    Sunday

--- code/COMPONENTS/--CMS/attributes.js
+++ code/COMPONENTS/--CMS/attributes.js
@@ -1,6 +1,6 @@
 // CMS collection lists
 // Apply attributes to individual CMS items
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
   function applyAttributesToItems(selector, attributeName) {
     const items = document.querySelectorAll(selector); // Select all items
 
@@ -10,21 +10,17 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     items.forEach((item) => {
-      const attributeField = item.getAttribute("cms-item"); // Use the cms-item attribute
+      const attributeField = item.getAttribute('cms-item'); // Use the cms-item attribute
       if (attributeField) {
         // Check if the cms-item attribute exists
         item.setAttribute(attributeName, attributeField); // Set the custom attribute
         console.log(`Set ${attributeName} to ${attributeField} for item`, item);
       } else {
-        console.warn("cms-item attribute not found in item:", item); // Warn if cms-item is not found
+        console.warn('cms-item attribute not found in item:', item); // Warn if cms-item is not found
       }
     });
   }
 
   // Apply attributes to project, process, and services items
-  applyAttributesToItems(".project--list-item", "project--attribute");
-  applyAttributesToItems(".process--list-item", "process--attribute");
-  applyAttributesToItems(".accordion--list-item", "services--attribute");
-  applyAttributesToItems(".office--list-item", "office--attribute");
-  applyAttributesToItems(".team--list-item", "team--attribute");
+  applyAttributesToItems('.footer--contact-links-list-item', 'teams--attribute');
 });
